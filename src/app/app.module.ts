15c4cab2eb603a2364ed8c75393f7f53b0a529a9
David Weidenbenner <44938475+comic-man@users.noreply.github.com>
2021-12-18 15:08:58 -0600

    f

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,35 @@ import { AppComponent } from './app.component';
 import { ToDoComponent } from './to-do/to-do.component';
 import { TodoItemComponent } from './todo-item/todo-item.component';
 
+import { AngularFireModule } from '@angular/fire'
+import { AngularFireBaseModule} from '@angular/fire/database';
+import { initializeApp } from 'firebase/app';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBKsvJzBFP64w3jE-UyeYIAUOKABy-k5Ow",
+  authDomain: "todolist-a6062.firebaseapp.com",
+  databaseURL: "https://todolist-a6062-default-rtdb.firebaseio.com",
+  projectId: "todolist-a6062",
+  storageBucket: "todolist-a6062.appspot.com",
+  messagingSenderId: "750256381334",
+  appId: "1:750256381334:web:a8700b796ca042674091b7",
+  measurementId: "G-YQXWF2PGMY"
+};
+const app = initializeApp(firebaseConfig);
+
 @NgModule({
   declarations: [
     AppComponent,
     ToDoComponent,
-    TodoItemComponent
+    TodoItemComponent,
+
+    AngularFireModule.intializeApp(firebaseConfig),
+    AngularFireBaseModule
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
