f0d311d08ef9c3d35bb03b7efaf0e2629b2dc3fa
David Weidenbenner <44938475+comic-man@users.noreply.github.com>
2021-12-18 13:48:38 -0600

    p

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToDoComponent } from './to-do/to-do.component';
+import { TodoItemComponent } from './todo-item/todo-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToDoComponent,
+    TodoItemComponent
   ],
   imports: [
     BrowserModule,
